4be218ea003618a77c31ae6a2bf66d7d234c634b
Lingfeng Yang <lingfeng.yang@mail.utoronto.ca>
2022-08-16 21:52:39 -0400

    about page about paragraph

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "../styles/About.scss";
+import profile_pic from "../assets/images/profile_pic.jpg";
 
 export const About = () => {
   return (
     <div className="background">
-      <div className="about-page">
-        <h1>About</h1>
+      <h1>About</h1>
+      <div className="title-head">
+        <div className="about-page">
+          <p class="b2">Nice to meet you, I'm Mike</p>
+          <div class="b3">
+            sasjhgsa gkjhsadg kjashdg ksajdg salkhg salkdhg kasjdh gkjash dg
+          </div>
+        </div>
+        <img src={profile_pic} alt="profile" />
       </div>
     </div>
   );
